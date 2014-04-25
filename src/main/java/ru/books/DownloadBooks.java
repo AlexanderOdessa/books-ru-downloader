c173846261452ba0d96b15fab2afe0cd1f0b18dc
Artem Pronchakov <artem.pronchakov@gmail.com>
2014-04-26 02:03:21 +0400

    Refactoring, logging

--- src/main/java/ru/books/DownloadBooks.java
+++ src/main/java/ru/books/DownloadBooks.java
@@ -23,43 +23,24 @@ import java.net.URISyntaxException;
  */
 public class DownloadBooks {
 
-    private static final Logger log = LoggerFactory.getLogger(DownloadBooks.class);
-
-    public static final String GENERAL_LOCATION = "https://www.books.ru";
-    public static final String LOGIN_PAGE = GENERAL_LOCATION + "/member/login.php";
-    public static final String ORDER_ADDRESS_PREFIX = GENERAL_LOCATION + "/order.php?order=";
-    public static final String LOGIN_PARAM = "login";
-    public static final String PASSWORD_PARAM = "password";
-    public static final String GO_PARAM = "go";
-    public static final String MAGIC_PARAM_X = "x";
-    public static final String MAGIC_PARAM_Y = "y";
-    public static final String MAGIC_VALUE_X = "45";
-    public static final String MAGIC_VALUE_Y = "8";
-    public static final String DEFAULT_ENCODING = "UTF-8";
-    public static final String CSS_BOOKS_SELECTOR = "table.catalog > tbody > tr:not(:last-child)";
-    public static final String CSS_TITLE_SELECTOR = "p.title a";
-    public static final String CSS_URL_SELECTOR = "td.status a";
-    public static final String HREF_ATTR = "href";
-    public static final String BOOKS_CATALOG = "books/";
-    public static final String SEPARATOR = " : ";
-    public static final String DOT = ".";
-    public static final String SLASH = "/";
-    public static final String EQUAL = "=";
     public static final String EMPTY_STRING = "";
+    private static final Logger log = LoggerFactory.getLogger(DownloadBooks.class);
+    private static final String URL = "https://www.books.ru";
+    private static final String BOOKS_FOLDER_NAME = "books/";
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         if (args.length < 3 || args[0].isEmpty() || args[1].isEmpty() || args[2].isEmpty()) {
-            System.out.println("");
-            System.out.println("");
-            System.out.println("Usage: java -jar books-downloader.jar {orderId} {username} {password}");
-            System.out.println("");
+            log.info(EMPTY_STRING);
+            log.info(EMPTY_STRING);
+            log.info("Usage: java -jar books-downloader.jar {orderId} {username} {password}");
+            log.info(EMPTY_STRING);
             return;
         }
 
-        File booksDir = new File(BOOKS_CATALOG);
+        File booksDir = new File(BOOKS_FOLDER_NAME);
         if (!booksDir.exists()) {
             if (!booksDir.mkdir()) {
-                System.out.println("Error: Cannot create directory " + BOOKS_CATALOG);
+                log.error("Error: Cannot create directory '{}'", BOOKS_FOLDER_NAME);
                 return;
             }
         }
@@ -71,47 +52,48 @@ public class DownloadBooks {
 
         HttpUriRequest login = RequestBuilder
                 .post()
-                .setUri(new URI(LOGIN_PAGE))
-                .addParameter(LOGIN_PARAM, args[1]).addParameter(PASSWORD_PARAM, args[2])
-                .addParameter(GO_PARAM, LOGIN_PARAM)
-                .addParameter(MAGIC_PARAM_X, MAGIC_VALUE_X)
-                .addParameter(MAGIC_PARAM_Y, MAGIC_VALUE_Y)
+                .setUri(new URI(URL + "/member/login.php"))
+                .addParameter("login", args[1])
+                .addParameter("password", args[2])
+                .addParameter("go", "login")
+                .addParameter("x", "45")
+                .addParameter("y", "8")
                 .build();
         CloseableHttpResponse loginResponse = httpclient.execute(login);
 
         if (loginResponse.getStatusLine().getStatusCode() == 302) {
-            System.out.println("Login OK");
+            log.info("Login OK");
         } else {
-            System.out.println("Login failed");
+            log.info("Login failed");
             return;
         }
 
         loginResponse.close();
 
-        HttpGet httpGet = new HttpGet(ORDER_ADDRESS_PREFIX + args[0]);
+        HttpGet httpGet = new HttpGet(URL + "/order.php?order=" + args[0]);
         CloseableHttpResponse booksResponse = httpclient.execute(httpGet);
 
-        Document doc = Jsoup.parse(booksResponse.getEntity().getContent(), DEFAULT_ENCODING, EMPTY_STRING);
-        Elements res = doc.select(CSS_BOOKS_SELECTOR);
+        Document doc = Jsoup.parse(booksResponse.getEntity().getContent(), "UTF-8", EMPTY_STRING);
+        Elements res = doc.select("table.catalog > tbody > tr:not(:last-child)");
 
-        System.out.println("Found " + res.size() + " books");
+        log.info("Found {} books", res.size());
 
         int i = 1;
         for (Element element : res) {
             try {
-                String title = element.select(CSS_TITLE_SELECTOR).text().replaceAll(SLASH, EMPTY_STRING);
+                String title = element.select("p.title a").text().replaceAll("/", EMPTY_STRING);
 
-                Elements urls = element.select(CSS_URL_SELECTOR);
+                Elements urls = element.select("td.status a");
                 for (Element url : urls) {
-                    String bookUrl = GENERAL_LOCATION + url.attr(HREF_ATTR).split(EQUAL)[1];
+                    String bookUrl = URL + url.attr("href").split("=")[1];
                     String suffix = url.text();
 
                     HttpGet bookGet = new HttpGet(bookUrl);
                     CloseableHttpResponse fileResponse = httpclient.execute(bookGet);
 
                     BufferedInputStream inputStraem = new BufferedInputStream(fileResponse.getEntity().getContent());
-                    String filename = (title.length() > 120 ? title.substring(0, 119) : title) + DOT + suffix;
-                    BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(BOOKS_CATALOG + filename));
+                    String filename = (title.length() > 120 ? title.substring(0, 119) : title) + "." + suffix;
+                    BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(BOOKS_FOLDER_NAME + filename));
 
                     int b;
                     while ((b = inputStraem.read()) != -1) {
@@ -120,7 +102,7 @@ public class DownloadBooks {
 
                     inputStraem.close();
                     outputStream.close();
-                    System.out.println(i + SEPARATOR + filename);
+                    log.info("{} : {}", i, filename);
 
                     fileResponse.close();
                 }
@@ -130,6 +112,8 @@ public class DownloadBooks {
                 i++;
             }
         }
+
+        log.info("All books were downloaded");
 
         booksResponse.close();
         httpclient.close();
