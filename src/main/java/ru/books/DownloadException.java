17e9f1e6fcf079e37cc0a128fa381382d635e901
Artem Pronchakov <artem.pronchakov@gmail.com>
2014-04-27 01:18:56 +0400

    gitignore fix

---
+++ src/main/java/ru/books/DownloadException.java
@@ -0,0 +1,12 @@
+package ru.books;
+
+/**
+ * @author Artem Pronchakov (artem.pronchakov@gmail.com)
+ */
+public class DownloadException extends Exception {
+
+    public DownloadException(String message) {
+        super(message);
+    }
+
+}
